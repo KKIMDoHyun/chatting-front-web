6f2d43dd084188e420a7ea37abbf536f98d7d961
KKIMDoHyun <kdh5998@naver.com>
2024-01-06 13:01:11 +0900

    [chore] tailwind 설치 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "@routers/Router";
 
-import "./App.css";
-
 export const App: React.FC = () => {
   return <RouterProvider router={router()} />;
 };
