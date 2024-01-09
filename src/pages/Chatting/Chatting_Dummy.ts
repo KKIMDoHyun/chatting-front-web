3529aa3c12099d15fc2d41dce181b413e8dfc854
KKIMDoHyun <kdh5998@naver.com>
2024-01-09 17:55:28 +0900

    [feat] 채팅 목록 구현 (+더미 데이터)

---
+++ src/pages/Chatting/Chatting_Dummy.ts
@@ -0,0 +1,14 @@
+export const Chatting_Dummy = [
+  {
+    id: 1,
+    sender: "조현준",
+    sendDate: "2024-01-08 12:05",
+    message: "안녕하세요",
+  },
+  {
+    id: 2,
+    sender: "조현준",
+    sendDate: "2024-01-08 12:12",
+    message: "안녕하세요 반가워요 나는 바보에요ㅋㅋㅋㅋㅋㅋ",
+  },
+];
