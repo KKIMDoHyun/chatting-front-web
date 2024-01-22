ae5632757589c3538f0bb720a34136ce265a2ec0
KKIMDoHyun <kdh5998@naver.com>
2024-01-22 16:24:53 +0900

    [feat] websocket의 메시지에 room 추가

--- src/components/Websocket/WebsocketProvider.tsx
+++ src/components/Websocket/WebsocketProvider.tsx
@@ -2,21 +2,27 @@
 import { createContext, useEffect, useRef, useState } from "react";
 
 type TMessage = {
-  type: string;
-  room?: string;
-  text?: string;
-  rooms?: string[];
+  room: string; // 채팅방 이름
+  user: string; // 사용자 이름
+  message: string; // 메시지 내용
+  timestamp: string[]; // 메시지 시간
 };
+type TRoom = {
+  name: string; // 채팅방 이름
+  messages: TMessage[]; // 채팅방의 메시지 목록
+};
+
 type WebSocketContextProps = {
-  rooms: string[];
+  rooms: TRoom[];
   messages: TMessage[];
-  sendMessage: (room: string, msg: string) => void;
+  getRooms: () => void;
+  getMessages: (room: string) => void;
+  sendMessage: (room: string, user: string, message: string) => void;
 };
-export const WebSocketContext = createContext<WebSocketContextProps>({
-  rooms: [],
-  messages: [],
-  sendMessage: () => {},
-});
+
+export const WebSocketContext = createContext<WebSocketContextProps | null>(
+  null
+);
 
 type WebsocketProviderProps = {
   children: React.ReactNode;
@@ -24,7 +30,7 @@ type WebsocketProviderProps = {
 export const WebsocketProvider: React.FC<WebsocketProviderProps> = ({
   children,
 }) => {
-  const [rooms, setRooms] = useState<string[]>([]);
+  const [rooms, setRooms] = useState<TRoom[]>([]);
   const [messages, setMessages] = useState<TMessage[]>([]);
   const socketRef = useRef<WebSocket | null>(null);
 
@@ -36,14 +42,13 @@ export const WebsocketProvider: React.FC<WebsocketProviderProps> = ({
     };
 
     socketRef.current.onmessage = (event) => {
-      const message = JSON.parse(event.data);
+      const response = JSON.parse(event.data);
 
-      if (message.type === "rooms") {
-        // room list를 받았을 때
-        setRooms(message.rooms);
-      } else if (message.type === "message") {
-        // 메시지를 받았을 때
-        setMessages((prevMessages) => [...prevMessages, message]);
+      if (response.type === "roomList") {
+        setRooms(response.rooms);
+      } else if (response.type === "messageList") {
+        // [TODO] 수정
+        setMessages(response.messages);
       }
     };
 
@@ -54,16 +59,35 @@ export const WebsocketProvider: React.FC<WebsocketProviderProps> = ({
     };
   }, []);
 
-  const sendMessage = (room: string, message: string) => {
-    if (socketRef.current) {
-      socketRef.current.send(
-        JSON.stringify({ type: "message", room, text: message })
-      );
-    }
+  const getRooms = () => {
+    const message = {
+      type: "getRooms",
+    };
+    socketRef.current?.send(JSON.stringify(message));
+  };
+
+  const getMessages = (room: string) => {
+    const message = {
+      type: "getMessages",
+      room: room,
+    };
+    socketRef.current?.send(JSON.stringify(message));
+  };
+
+  const sendMessage = (room: string, user: string, text: string) => {
+    const message = {
+      type: "message",
+      room: room,
+      user: user,
+      text: text,
+    };
+    socketRef.current?.send(JSON.stringify(message));
   };
 
   return (
-    <WebSocketContext.Provider value={{ rooms, messages, sendMessage }}>
+    <WebSocketContext.Provider
+      value={{ rooms, messages, getRooms, getMessages, sendMessage }}
+    >
       {children}
     </WebSocketContext.Provider>
   );
