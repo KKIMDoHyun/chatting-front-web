9038cbbbedb5de7634b65f0c6b561c29cf10b1f0
KKIMDoHyun <kdh5998@naver.com>
2024-02-22 15:06:26 +0900

    [feat] 채팅 메시지 오면 스크롤 맨 아래로 가도록 구현

--- src/pages/Chatting/Components/ChattingMessage.tsx
+++ src/pages/Chatting/Components/ChattingMessage.tsx
@@ -1,27 +1,52 @@
+import React from "react";
+
 import { useAtomValue } from "jotai";
 
 import { TChatMessage } from "@typings/WebsocketMessage";
 
-import { UserAtom } from "@stores/UserStore";
+import { UserAtom, User_Dummy } from "@stores/UserStore";
 
-type ChatttingMessageProps = {
+type ChattingMessageProps = {
   chatting: TChatMessage[];
 };
 
-export const ChattingMessage = ({ chatting }: ChatttingMessageProps) => {
+export const ChattingMessage = ({ chatting }: ChattingMessageProps) => {
   const user = useAtomValue(UserAtom);
+  const messageEndRef = React.useRef<HTMLDivElement | null>(null);
+
+  const userMapping = (userId: number) => {
+    const user = User_Dummy.filter((u) => u.id === userId);
+    return user[0].name;
+  };
+
+  React.useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollTop = messageEndRef.current.scrollHeight;
+    }
+  }, [chatting]);
+
   return (
-    <div className="flex flex-col overflow-x-hidden overflow-y-auto p-[20px] gap-[10px]">
+    <div
+      ref={messageEndRef}
+      className="flex flex-col overflow-x-hidden overflow-y-auto p-[20px] gap-[10px]"
+    >
       {chatting.map((chat) => (
         <div
           key={chat.id}
-          className={`${
+          className={`flex ${
             chat.sender === user.id
-              ? "bg-green-300 self-end"
-              : "bg-gray-200 self-start"
-          } max-w-[484px] p-[8px] rounded-2xl text-[14px]`}
+              ? "self-end flex-row-reverse"
+              : "self-start flex-row"
+          } `}
         >
-          {chat.content}
+          {chat.sender !== user.id && <div>{userMapping(chat.sender)}</div>}
+          <div
+            className={`${
+              chat.sender === user.id ? "bg-green-300" : "bg-gray-200"
+            } rounded-2xl max-w-[484px] p-[8px] text-[14px]`}
+          >
+            {chat.content}
+          </div>
         </div>
       ))}
     </div>
