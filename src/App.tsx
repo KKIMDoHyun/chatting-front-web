be40e4b09135bc966e9a7eb3bdf157e6a31ae45c
KKIMDoHyun <kdh5998@naver.com>
2024-04-16 14:55:04 +0900

    [chore] jotai의 Provider 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "jotai";
 
 import { router } from "@routers/Router";
 
@@ -10,8 +11,10 @@ export const App: React.FC = () => {
   });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router()} />
-    </QueryClientProvider>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router()} />
+      </QueryClientProvider>
+    </Provider>
   );
 };
