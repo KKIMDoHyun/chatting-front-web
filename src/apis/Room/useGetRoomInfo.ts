e5f19b740dfb33fab294e0e9d9b7826baf63f287
KKIMDoHyun <kdh5998@naver.com>
2024-09-15 00:39:08 +0900

    [chore] 채팅방 정보 api에 placeholder 설정

--- src/apis/Room/useGetRoomInfo.ts
+++ src/apis/Room/useGetRoomInfo.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 import { instance } from "@apis/AxiosInstance";
 import { QUERY_KEYS } from "@apis/QUERY_KEYS";
@@ -26,5 +26,6 @@ export const useGetRoomInfo = (params: GetRoomInfoReq) => {
   return useQuery<GetRoomInfoRes, TErrorRes>({
     queryKey: QUERY_KEYS.ROOM.detail(JSON.stringify(params)),
     queryFn: () => getRoomInfo(params),
+    placeholderData: keepPreviousData,
   });
 };
