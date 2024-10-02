d0223844d3a7220641df3176cf4f2a9024ccc91a
KKIMDoHyun <kdh5998@naver.com>
2024-10-02 13:00:07 +0900

    [feat] 메세지의 타입(IMAGE/FILE/TEXT)별 노출 형식 구현

--- src/pages/Chat/Components/ChatView/MessageItem.tsx
+++ src/pages/Chat/Components/ChatView/MessageItem.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Download, FileIcon } from "lucide-react";
+
 import { TChatMessageDetail } from "@typings/Chat";
 
 export const MessageItem: React.FC<{
@@ -31,21 +33,56 @@ export const MessageItem: React.FC<{
     }
 
     const renderMessageContent = () => {
-      if (message.messageType === "IMAGE" && message.files.length > 0) {
+      // 메세지가 이미지일 경우
+      if (message.messageType === "IMAGE") {
         return (
           <img
             src={message.files[0].url}
             alt="Sent image"
-            className="max-h-[300px] max-w-[300px] rounded-2xl object-contain"
+            className="max-h-[200px] max-w-[200px] rounded-2xl object-contain"
           />
         );
       }
+      // 메세지가 파일일 경우
+      else if (message.messageType === "FILE") {
+        const file = message.files[0];
+        const fileSize = (file.size / 1024).toFixed(1) + " KB";
+
+        const handleDownload = () => {
+          const link = document.createElement("a");
+          link.href = file.url;
+          link.download = file.name;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        };
+
+        return (
+          <div className="flex max-w-[300px] flex-col rounded-lg bg-white p-3 shadow-md">
+            <div className="flex items-center gap-3">
+              <div className="h-10 w-10 self-center">
+                <FileIcon size={30} />
+              </div>
+              <div className="flex flex-col overflow-hidden">
+                <span className="truncate text-sm font-semibold">
+                  {file.name}
+                </span>
+                <span className="text-xs text-gray-500">{fileSize}</span>
+              </div>
+            </div>
+            <button
+              onClick={handleDownload}
+              className="flex items-center gap-2 self-end rounded-md px-2 py-1 text-sm hover:bg-gray-50"
+            >
+              <Download size={16} />
+              저장
+            </button>
+          </div>
+        );
+      }
+      // 메세지가 TEXT일 경우
       return (
-        <div
-          className={`${
-            isCurrentUser ? "bg-green-300" : "bg-gray-200"
-          } overflow-wrap-anywhere w-fit max-w-[484px] whitespace-pre-wrap break-words rounded-2xl p-[8px] text-[14px]`}
-        >
+        <div className="w-fit max-w-[484px] whitespace-pre-wrap break-words break-keep rounded-lg bg-gray-200 p-[8px] text-[14px]">
           {message.plainText}
         </div>
       );
