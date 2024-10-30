bf081743d86bc8dd70c1a56139ef5f4cfe631cdc
KKIMDoHyun <kdh5998@naver.com>
2024-10-30 14:18:34 +0900

    [feat] 관리자 메세지에서 유저 매핑하여 이름으로 변환하여 출력

--- src/pages/Chat/Components/ChatView/MessageItem/SystemMessage.tsx
+++ src/pages/Chat/Components/ChatView/MessageItem/SystemMessage.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 
+import { useAtomValue } from "jotai";
+
+import { RoomMemberHistoryAtom } from "@stores/RoomStore";
+
+const MEMBER_ID_PATTERN = /\{([^}]+)\}/g;
+
 type SystemMessageProps = {
   plainText: string;
 };
 
-export const SystemMessage = React.memo(({ plainText }: SystemMessageProps) => (
-  <div className="flex justify-center py-5">
-    <span className="rounded-full bg-gray-100 px-4 py-1 text-xs font-semibold text-gray-500">
-      {plainText}
-    </span>
-  </div>
-));
+export const SystemMessage = React.memo(({ plainText }: SystemMessageProps) => {
+  const memberHistory = useAtomValue(RoomMemberHistoryAtom);
+  const memberMap = new Map(
+    memberHistory.map((member) => [member.id, member.name])
+  );
+
+  const formattedMessage = plainText.replace(MEMBER_ID_PATTERN, (_, id) => {
+    return memberMap.get(id) || id;
+  });
+
+  return (
+    <div className="flex justify-center py-5">
+      <div className="flex w-full max-w-[700px] justify-center">
+        <span className="break-words rounded-xl bg-gray-100 px-4 py-1 text-center text-xs font-semibold text-gray-500">
+          {formattedMessage}
+        </span>
+      </div>
+    </div>
+  );
+});
