aef33efca50dcd87f5d36fd6a55a08e8b570d56c
KKIMDoHyun <kdh5998@naver.com>
2024-11-28 15:44:04 +0900

    [feat] 채팅창에 포커싱 되었을 때마다 mark as read api 호출

--- src/hooks/useChatRoomReadStatus.ts
+++ src/hooks/useChatRoomReadStatus.ts
@@ -12,14 +12,28 @@ export const useChatRoomReadStatus = (
   const prevRoomIdRef = useRef<string | null>(null);
 
   useEffect(() => {
+    const handleFocus = () => {
+      if (roomId && messages.length > 0) {
+        checkRead({ roomId });
+      }
+    };
+
     // roomId가 비어있거나 이전 roomId와 다를 때 체크
-    if (prevRoomIdRef.current !== null && prevRoomIdRef.current !== roomId) {
+    if (prevRoomIdRef.current !== null && prevRoomIdRef.current === roomId) {
       // 이전 채팅방에서 나가는 경우
       if (messages.length > 0) {
         checkRead({ roomId: prevRoomIdRef.current });
       }
     }
+
     // 현재 roomId 저장
-    prevRoomIdRef.current = roomId ?? null; // roomId가 undefined일 경우 null로 저장
+    prevRoomIdRef.current = roomId ?? null;
+
+    // 포커스 이벤트 리스너 등록
+    window.addEventListener("focus", handleFocus);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+    };
   }, [roomId, messages, checkRead]);
 };
